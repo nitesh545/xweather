961939c330eac25428f81569b99e1e5cab6f1c27
nitecse <hitman12457868@gmail.com>
2024-09-29 22:53:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import {Box, Button} from "@mui/material";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [temp, setTemp] = useState(0.0);
+	const [humidity, setHumidity] = useState(0.0);
+	const [condition, setCondition] = useState('');
+	const [windSpeed, setWindSpeed] = useState(0.0);
+	const [cityName, setCityName] = useState('');
+	const [searching, setSearhing] = useState(false);
+
+	function fetchPlaces(city) {
+		fetch(`https://api.weatherapi.com/v1/current.json?key=ee2961071b524586b39161743242909&q=${city}`)
+			.then(res => res.json())
+			.then((data) => {
+				console.log(data.current);
+				setTemp(data.current.temp_c);
+				setHumidity(data.current.humidity);
+				setCondition(data.current.condition.text);
+				setWindSpeed(data.current.wind_kph);
+				setSearhing(true);
+			})
+			.catch(err => {
+				alert("Failed to fetch weather data");
+			});
+	}
+
+	useEffect(() => {
+		fetchPlaces();
+	}, []);
+
+	return (
+		<div className="App">
+			<Box sx={{display: 'flex', justifyContent: 'center', padding: '5rem'}}>
+				<input type='text' placeholder='Enter city name' className='search-bar' onChange={(e) => {
+					setCityName(e.target.value);
+				}}/>
+				<button className='search-btn' onClick={() => {
+					console.log(cityName);
+					fetchPlaces(cityName);
+				}}>Search
+				</button>
+			</Box>
+			{
+				!searching ? (<p>Loading...</p>) : (
+
+					<div className='weather-cards'>
+						<div className='weather-card'>
+							<h4>Temperature</h4>
+							<h5>{temp}</h5>
+						</div>
+						<div className='weather-card'>
+							<h4>Humidity</h4>
+							<h5>{humidity}</h5>
+						</div>
+						<div className='weather-card'>
+							<h4>Condition</h4>
+							<h5>{condition}</h5>
+						</div>
+						<div className='weather-card'>
+							<h4>Wind Speed</h4>
+							<h5>{windSpeed}</h5>
+						</div>
+					</div>)
+			}
+		</div>
+	);
 }
 
 export default App;
